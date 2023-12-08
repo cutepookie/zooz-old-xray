c65cbbac249a64cd25061c02eb93a272cc18a9fb
linulinu <moonfox806@gmail.com>
2023-12-08 13:11:32 +0700

    pr 1

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ export WB='\e[37;1m'
 
 if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
-		#exit 1
+		exit 1
 fi
 if [ "$(systemd-detect-virt)" == "openvz" ]; then
 		echo "OpenVZ is not supported"
